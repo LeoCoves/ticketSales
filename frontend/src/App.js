1d87e01b73e58a41db149debec8091aa9dd06507
leo <145478507+leocovess@users.noreply.github.com>
2025-01-27 02:17:57 +0100

    Axios installed, Node server configurated and database no-created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,69 @@
+import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Axios from 'axios';
 
 function App() {
+
+  const [nombre, setNombre] = useState('');
+  const [evento, setEvento] = useState('');
+  const [fecha, setFecha] = useState('');
+  const [edad, setEdad] = useState(0);
+  const [descripcion, setDescripcion] = useState('');
+
+  const add = () => {
+    Axios.post('http://localhost:3001/create', {
+      nombre: nombre,
+      evento: evento,
+      fecha: fecha,
+      edad: edad,
+      descripcion: descripcion
+    }).then(()=>{
+      alert("Process completed");
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <div className="datos">
+
+        <label>
+          Compañia: 
+          <input onChange={(event)=>{
+            setNombre(event.target.value);
+          }} type="text" />
+        </label>
+
+        <label>
+          Evento: 
+          <input onChange={(event)=>{
+            setEvento(event.target.value);
+          }} type="text" />
+        </label>
+
+        <label>Fecha: 
+          <input onChange={(event)=>{
+            setFecha(event.target.value);
+          }} type="text" />
+        </label>
+
+        <label>
+          Edad minima: 
+          <input onChange={(event)=>{
+            setEdad(event.target.value);
+          }} type="number" />
+        </label>
+
+        <label>Descripcion: 
+          <input onChange={(event)=>{
+            setDescripcion(event.target.value);
+          }} type="text" />
+        </label>
+
+        <button onClick={add}>
+          Enviar
+        </button>
+      </div>
     </div>
   );
 }
